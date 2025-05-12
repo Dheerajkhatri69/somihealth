1de488cb70b4e813b104d1c3f861d28a778638a5
dheeraj khatri <dheerajkum838@gmail.com>
2025-05-12 14:44:29 +0500

    change in api

--- app/api/imagekit-auth/route.js
+++ app/api/imagekit-auth/route.js
@@ -1,9 +1,9 @@
 import ImageKit from "imagekit";
 
 const imagekit = new ImageKit({
-  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
-  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
-  urlEndpoint: process.env.NEXT_PUBLIC_URL_ENDPOINT,
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY || 'public_HY7YY/rIWqQyH/VxGRhxOdOfE0g=',
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY || 'private_OR8Z9D0K5v6AJKWVASqn003QOKg=',
+  urlEndpoint: process.env.NEXT_PUBLIC_URL_ENDPOINT || 'https://ik.imagekit.io/3a9nocke4',
 });
 
 export async function GET() {
