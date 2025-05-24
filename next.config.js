1c48cf8758d8b88842654678ce6b0abea6397ae4
dheeraj khatri <dheerajkum838@gmail.com>
2025-05-25 00:02:29 +0500

    logo

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true, // Disable image optimization
     domains: ['ik.imagekit.io', 'res.cloudinary.com'], 
   },
   reactStrictMode: false,
